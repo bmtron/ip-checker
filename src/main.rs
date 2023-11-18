e062ec4a33163685231251ebbdd069331743ffa5
Brendan <bmmeehan3@gmail.com>
2023-11-18 12:27:21 -0500

    first pass, POC on async requests in rust

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::io::Read;
+use tokio::io::AsyncReadExt;
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+    test_request().await;
+}
+
+async fn test_request() -> Result<(), Box<dyn std::error::Error>> {
+    let body = reqwest::get("http://localhost:8080/ip")
+        .await?
+        .text()
+        .await?;
+
+    println!("body: {:?}", body);
+
+    Ok(())
+}
