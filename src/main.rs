1a966d9459ff513deda471c28b33f00130e75e9f
Brendan <bmmeehan3@gmail.com>
2023-11-19 15:52:40 -0500

    added runlogging, fixed datetime formatting

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,30 @@ struct IpAddress {
     isactive: bool,
     datecreated: String
 }
+
+#[derive(Serialize, Deserialize)]
+struct IpRunLog {
+    iprunlogid: u8,
+    rundate: String,
+    ipupdated: bool
+}
 impl From<String> for IpAddress {
     fn from(value: String) -> Self {
         IpAddress {
             ipaddressid: 0,
             ipaddress: value,
             isactive: true,
-            datecreated: Local::now().format("%Y-%m-%d][%H:%M:%S").to_string()
+            datecreated: Local::now().format("%Y-%m-%d %H:%M:%S").to_string()
+        }
+    }
+}
+
+impl From<bool> for IpRunLog {
+    fn from(value: bool) -> Self {
+        IpRunLog {
+            iprunlogid: 0,
+            rundate: Local::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+            ipupdated: value
         }
     }
 }
@@ -30,7 +47,7 @@ struct ApiResultWrapper<T> {
 }
 #[tokio::main]
 async fn main() -> Result<()>{
-    println!("Hello, world!");
+    let run_log;
 
     let result = get_active_ip().await.expect("TODO: panic message");
     let real_public_ip = get_real_public_ip().await.expect("TODO: PANICKING");
@@ -39,14 +56,16 @@ async fn main() -> Result<()>{
 
     if real_public_ip == result.Value.get(0).unwrap().ipaddress {
         println!("{}", String::from("ips match, success"));
+        run_log = IpRunLog::from(false);
     } else {
         println!("{}", String::from("ipmismatch, update database to new publicip"));
+        run_log = IpRunLog::from(true);
         deactivate_ip_records().await.expect("Panik");
         let new_ip = IpAddress::from(real_public_ip);
         insert_new_ip_record(&new_ip).await.expect("PANIK");
         emailer::send_ips_changed_alert().await.expect("PANIK");
     }
-
+    insert_new_ip_runlog(&run_log).await.expect("PANIK");
     Ok(())
 }
 
@@ -90,18 +109,14 @@ async fn insert_new_ip_record(ipaddress: &IpAddress) -> std::result::Result<(), 
 
     Ok(())
 }
-/*async fn test_request() -> Result<(), Box<dyn std::error::Error>> {
-    let body = reqwest::get("http://localhost:8080/ip")
-        .await?
-        .text()
-        .await?;
 
-    println!("body: {:?}", body);
-    let body = reqwest::get("http://ipinfo.io/ip")
-        .await?
-        .text()
+async fn insert_new_ip_runlog(ip_run_log: &IpRunLog) -> std::result::Result<(), Box<dyn std::error::Error>> {
+    let serialized_body = serde_json::to_string(&ip_run_log).unwrap();
+    let client = reqwest::Client::new();
+    let res = client.post("http://localhost:8080/ip/runlog/new")
+        .body(serialized_body)
+        .send()
         .await?;
-    println!("ipaddress: {:?}", body);
 
     Ok(())
-}*/
+}
